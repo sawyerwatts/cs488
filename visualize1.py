93bd26929690016c6f9fb2435c7c77fb950363ca
Sawyer Watts <sawyer.watts@gmail.com>
2020-06-03 15:34:31 -0700

    Init this branch.
    I will be using Git to move files easily so this branch exists.

--- visualize1.py
+++ visualize1.py
@@ -1,5 +1,8 @@
 import datetime
 import pandas
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
 from query_setup import collection
 from epoch_converter import convert_epoch
 
@@ -8,6 +11,7 @@ from epoch_converter import convert_epoch
 # Functions
 ###########
 
+"""
 # Use to build an entry of a DataFrame with pandas.DataFrame.append().
 def make_row(datetimerecorded, stationname, frequency, use_weekday=False):
     if use_weekday:
@@ -21,7 +25,7 @@ def make_row(datetimerecorded, stationname, frequency, use_weekday=False):
         elif weekday == 7: weekday = "Sunday"
 
         return {
-            "weekday": datetimerecorded,
+            "weekday": weekday,
             "stationname": stationname,
             "frequency": frequency,
             "hour": datetimerecorded.hour
@@ -86,69 +90,49 @@ for key in comps:
                 ignore_index=True
                 )
 
+# """
+
+final = pandas.read_csv("data1.csv") # TODO: when I uncomment the above block, I need to delete this line and read file.
+alt = final.copy(deep=True) # TODO: delete this line
+alt["weekday"] = "Thursday" # TODO: delete this line
+final = final.append(alt, ignore_index=True) # TODO: delete this line
 print("The frequency of an entry going faster than 100 MPH by hour, station, and weekday:")
 print(final)
-
-final.to_csv("data1.csv", index=False)
 
 
 ###############################################################################
 # Visualize
 ###########
 
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-
-# TODO: make small multiples for each *day*.
-
-# TODO: have the subtitles be extracted from the data
-fig = make_subplots(rows=4, cols=2, subplot_titles=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
-fig.add_trace(
-    go.Scatter(
-        name="Monday",
-        x = [0, 1, 2, 3, 4], # TODO: replace with hour
-        y = ["Station A", "Station B", "Station C", "Station D", "Station E"], # TODO: replace with stations
-        mode="markers",
-        marker=dict(
-            size=15,
-            color=[5, 4, 3, 2, 1], # TODO: have this map the color to the frequency
-            colorscale="Viridis",
-            showscale=True,
-            line=dict(
-                    color="Black",
-                    width=2
+weekdays = list(set(final["weekday"].tolist()))
+fig = make_subplots(rows=len(weekdays), cols=1, subplot_titles=weekdays)
+for i in range(len(weekdays)):
+    row_counter = i + 1
+    fig.add_trace(
+        go.Scatter(
+            name=weekdays[i],
+            x = final["hour"],
+            y = final["stationname"],
+            mode="markers",
+            marker=dict(
+                size=15,
+                color= final["frequency"],
+                colorscale="Viridis",
+                showscale=True,
+                line=dict(
+                        color="Black",
+                        width=2
+                )
             )
-        )
-    ),
-    row=1, col=1
-)
-fig.update_xaxes(range=[-1, 24], row=1, col=1)
-
-fig.add_trace(
-    go.Scatter(
-        name="Tuesday",
-        x = [0, 1, 2, 3, 4], # TODO: replace with hour
-        y = ["Station A", "Station B", "Station C", "Station D", "Station E"], # TODO: replace with stations
-        mode="markers",
-        marker=dict(
-            size=15,
-            color=[5, 4, 3, 2, 1], # TODO: have this map the color to the frequency
-            colorscale="Viridis",
-            showscale=True,
-            line=dict(
-                    color="Black",
-                    width=2
-            )
-        )
-    ),
-    row=1, col=2
-)
-fig.update_xaxes(range=[-1, 24], row=1, col=2)
+        ),
+        row=row_counter, col=1
+    )
+    fig.update_xaxes(title_text="time (24-Hour)", range=[0, 23], row=row_counter, col=1)
 
 fig.update_layout(
     title="Frequencies of speeding (over 100 MPH) per Hour by Station",
     showlegend=False,
-    height=2000,
+    #height=750*len(weekdays),
     font=dict(
         size=18
     )
