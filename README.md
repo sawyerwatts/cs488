f08202f1a5c6dacb039aede66cae65362da6e141
Sawyer Watts <sawyer.watts@gmail.com>
2020-06-03 23:32:17 +0000

    Break visualize1 into two viles.

--- README.md
+++ README.md
@@ -129,8 +129,15 @@ full dataset is used.
 
 ## Visualizations
 
-The various files that follow the syntax regex(visualize[1-6]\.py) are
+The various files that follow the syntax regex(pre_visualize[1-6]\.py) are
 visualization scripts that go into more detail about the corresponding query.
+
+The various file(s) that follow the syntax regex(pre_visualize[1-6]\.py) are
+used to generate a CSV that can be visualized by file(s) with syntax
+regex(visualize[1-6]\.py). The pregenerated files live in
+visualizations.tar.gz. There was the option to execute these scripts as a
+single script per query and output an HTML file, but it has issues actually
+running well in a browser.
 
 1. This visualization will take the entries with a speed greater than 100 MPH
 and plot the time by the station to display the frequency of this high speed,
---
+++ pre_visualize1.py
@@ -0,0 +1,95 @@
+import datetime
+import pandas
+
+from query_setup import collection
+from epoch_converter import convert_epoch
+
+
+###############################################################################
+# Functions
+###########
+
+# Use to build an entry of a DataFrame with pandas.DataFrame.append().
+def make_row(datetimerecorded, stationname, frequency, use_weekday=False):
+    if use_weekday:
+        weekday = datetimerecorded.weekday()
+        if   weekday == 1: weekday = "Monday"
+        elif weekday == 2: weekday = "Tuesday"
+        elif weekday == 3: weekday = "Wednesday"
+        elif weekday == 4: weekday = "Thursday"
+        elif weekday == 5: weekday = "Friday"
+        elif weekday == 6: weekday = "Saturday"
+        elif weekday == 7: weekday = "Sunday"
+
+        return {
+            "weekday": weekday,
+            "stationname": stationname,
+            "frequency": frequency,
+            "hour": datetimerecorded.hour
+        }
+
+    else:
+        return {
+            "datetimerecorded": datetimerecorded,
+            "stationname": stationname,
+            "frequency": frequency
+        }
+
+
+###############################################################################
+# Verify and collected needed data
+##################################
+
+# Generate valid, which will use make_row to build a DataFrame of relevant data.
+valid = pandas.DataFrame()
+i = 0
+for document in collection.find({"recorded.speed" : {"$gt" : 100}}):
+    i = 1
+    epoch = document["recorded"]["datetimerecorded"]
+    datetimerecorded = convert_epoch(epoch / 1000, type="datetime")
+    stationname = document["location"]["stationname"]
+    speed = document["recorded"]["speed"]
+    if speed is not None:
+        valid = valid.append(make_row(datetimerecorded, stationname, speed), ignore_index=True)
+
+if i == 0: raise Exception("There is no data to work with; this will not run.")
+
+
+# Scrub the minutes and seconds off each record and track the frequency of the
+# values combined.
+valid["datetimerecorded"] = valid["datetimerecorded"].apply(lambda date: date.replace(second=0))
+valid["datetimerecorded"] = valid["datetimerecorded"].apply(lambda date: date.replace(minute=0))
+
+data = pandas.DataFrame()
+for datehour in valid["datetimerecorded"].unique():
+    for station in valid["stationname"].unique():
+        temp = valid[(valid["stationname"] == station) & (valid["datetimerecorded"] == datehour)]
+        data = data.append(make_row(datehour, station, len(temp.index)), ignore_index=True)
+
+
+# Condense the timeframe into a single week.
+comps = {}
+for index, row in data.iterrows():
+    key = (row["datetimerecorded"], row["stationname"])
+    if key not in comps:
+        comps[key] = {}
+        comps[key]["count"] = row["frequency"]
+    else:
+        comps[key]["count"] += row["frequency"]
+
+final = pandas.DataFrame()
+for key in comps:
+    datetimerecorded = key[0]
+    stationname = key[1]
+    freq = comps[key]["count"]
+    final = final.append(
+                make_row(datetimerecorded, stationname, freq, use_weekday=True),
+                ignore_index=True
+                )
+
+print("The frequency of an entry going faster than 100 MPH by hour, station, and weekday:")
+print(final)
+
+final.to_csv("visualize1.csv", index=False)
+
+
