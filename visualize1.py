70ae6080e0f55cf9e7018d9e4a66d3608414b70e
Sawyer Watts <sawyer.watts@gmail.com>
2020-06-03 05:21:01 +0000

    Transfer data to repo so I can easily tweak on my machine

--- visualize1.py
+++ visualize1.py
@@ -4,6 +4,10 @@ from query_setup import collection
 from epoch_converter import convert_epoch
 
 
+###############################################################################
+# Functions
+###########
+
 # Use to build an entry of a DataFrame with pandas.DataFrame.append().
 def make_row(datetimerecorded, stationname, frequency, use_weekday=False):
     if use_weekday:
@@ -30,6 +34,10 @@ def make_row(datetimerecorded, stationname, frequency, use_weekday=False):
             "frequency": frequency
         }
 
+
+###############################################################################
+# Verify and collected needed data
+##################################
 
 # Generate valid, which will use make_row to build a DataFrame of relevant data.
 valid = pandas.DataFrame()
@@ -81,6 +89,72 @@ for key in comps:
 print("The frequency of an entry going faster than 100 MPH by hour, station, and weekday:")
 print(final)
 
-# TODO: visualize
+final.to_csv("data1.csv", index=False)
 
+
+###############################################################################
+# Visualize
+###########
+
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+# TODO: make small multiples for each *day*.
+
+# TODO: have the subtitles be extracted from the data
+fig = make_subplots(rows=4, cols=2, subplot_titles=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
+fig.add_trace(
+    go.Scatter(
+        name="Monday",
+        x = [0, 1, 2, 3, 4], # TODO: replace with hour
+        y = ["Station A", "Station B", "Station C", "Station D", "Station E"], # TODO: replace with stations
+        mode="markers",
+        marker=dict(
+            size=15,
+            color=[5, 4, 3, 2, 1], # TODO: have this map the color to the frequency
+            colorscale="Viridis",
+            showscale=True,
+            line=dict(
+                    color="Black",
+                    width=2
+            )
+        )
+    ),
+    row=1, col=1
+)
+fig.update_xaxes(range=[-1, 24], row=1, col=1)
+
+fig.add_trace(
+    go.Scatter(
+        name="Tuesday",
+        x = [0, 1, 2, 3, 4], # TODO: replace with hour
+        y = ["Station A", "Station B", "Station C", "Station D", "Station E"], # TODO: replace with stations
+        mode="markers",
+        marker=dict(
+            size=15,
+            color=[5, 4, 3, 2, 1], # TODO: have this map the color to the frequency
+            colorscale="Viridis",
+            showscale=True,
+            line=dict(
+                    color="Black",
+                    width=2
+            )
+        )
+    ),
+    row=1, col=2
+)
+fig.update_xaxes(range=[-1, 24], row=1, col=2)
+
+fig.update_layout(
+    title="Frequencies of speeding (over 100 MPH) per Hour by Station",
+    showlegend=False,
+    height=2000,
+    font=dict(
+        size=18
+    )
+)
+
+fig.show()
+
+# fig.write_html("./vis1.html")
 
